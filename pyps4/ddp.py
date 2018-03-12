4cdfad13b2ce8eadd85189ba00b96f58cb397963
Henrik Nicolaisen <henrik.nicolaisen@linkmobility.com>
2018-03-12 13:16:37 +0100

    handle errors in broadcast better

--- pyps4/ddp.py
+++ pyps4/ddp.py
@@ -73,14 +73,14 @@ def _send_recv_msg(host, broadcast, msg, receive=True):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     except OSError as error:
         _LOGGER.error('failed to create socket, %s', error)
-        return
+        return [ None, host ]
     try:
         sock.bind((UDP_IP, UDP_PORT))
         sock.settimeout(3.0)
     except OSError as error:
         _LOGGER.error('failed to bind socket %s:%s, %s', UDP_IP, UDP_PORT, error)
         sock.close()
-        return
+        return [ None, host ]
 
     try:
         if broadcast:
@@ -97,7 +97,7 @@ def _send_recv_msg(host, broadcast, msg, receive=True):
     except OSError as error:
         _LOGGER.error('failed to send data using socket, %s', error)
         sock.close()
-        return
+        return [ None, host ]
 
 
 def _send_msg(host, broadcast, msg):
@@ -107,15 +107,16 @@ def _send_msg(host, broadcast, msg):
 
 def search(host=None, broadcast=True):
     """Discover PS4s."""
-    msg = get_ddp_search_message()
-    data, addr = _send_recv_msg(host, broadcast, msg)
-
     ps_list = []
-    data = parse_ddp_response(data.decode('utf-8'))
-    data[u'host-ip'] = addr[0]
-    ps_list.append(data)
-    return ps_list
+    msg = get_ddp_search_message()
 
+    data, addr = _send_recv_msg(host, broadcast, msg)
+    if data is not None:
+        data = parse_ddp_response(data.decode('utf-8'))
+        data[u'host-ip'] = addr[0]
+        ps_list.append(data)
+    return ps_list
+    
 
 def get_status(host):
     """Get status."""
